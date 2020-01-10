c63a8ee1a3f6792324b75d2e9e6b8cfeb13edb20
Pauline Vos <pvos88@gmail.com>
2020-01-10 16:26:30 +0100

    Add Spotify pre-save link

--- app/components/SubContent.js
+++ app/components/SubContent.js
@@ -2,11 +2,20 @@ import React from 'react';
 import SubscribeForm from './SubscribeForm'
 import SocialIcon from './SocialIcon'
 import { IoLogoInstagram, IoLogoTwitter } from 'react-icons/io'
+import { FaSpotify } from 'react-icons/fa'
 
 export default function SubContent() {
   return (
     <React.Fragment>
       <h2>31.01.2020</h2>
+      <a 
+        className='btn btn-fullWidth icon' 
+        href='https://www.toneden.io/mvsei-music-group-1/post/balans'
+        target='_blank'
+      >
+        <FaSpotify size={24} />
+        <span>Pre-save on Spotify</span>
+      </a>
       <SubscribeForm />
       <div className="socialIcons">
         <SocialIcon link='http://instagram.com/yungnnelg'>
