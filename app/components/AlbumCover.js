ec852e8859e2a903297f51938ff6640851a12578
Pauline Vos <pvos88@gmail.com>
2020-01-10 17:05:24 +0100

    Remove click behavior
    As users didn't seem to understand they had to click through to the next
    state.

--- app/components/AlbumCover.js
+++ app/components/AlbumCover.js
@@ -2,40 +2,16 @@ import React from 'react'
 import SubContent from './SubContent'
 import balans from '../assets/balans.png'
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      clicked: false
-    }
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    this.setState({
-      clicked: true
-    })
-  }
-
-  render() {
-    const {clicked} = this.state
-
-    return (
-      <div 
-        className={`albumCover ${clicked ? "alignBaseline" : "alignCenter clickable"}`}
-        onClick={!clicked ? this.handleClick : () => {}}
-      >
-        <div className='content'>
-          {/*This is a placeholder*/}
-          <img src={balans} alt='Yung Nnelg: Balans' /> 
-
-          {clicked && (
-            <SubContent />
-          )}
-        </div>
+export default function AlbumCover() {
+  return (
+    <div 
+      className='albumCover alignBaseline'
+    >
+      <div className='content'>
+        {/*This is a placeholder*/}
+        <img src={balans} alt='Yung Nnelg: Balans' /> 
+        <SubContent />
       </div>
-    )
-  }
+    </div>
+  )
 }
